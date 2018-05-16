2a9102a8bba1d6f5d236d2ebecd83b74a82e5061
Rob Bianca <robo@Robs-Air.fios-router.home>
2018-05-16 18:02:08 -0400

    Project1 first commit

---
+++ project1.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var int = 0
+var doub = 0.0
+var flo = 0.0
+var boo = true
+
+func cube(length: Double) -> Double {
+    let volume = length * length * length
+    return volume
+}
